51e5068e222da24afcd72381f2cf53d8dbdb77b1
omobosteven <omobosteven@gmail.com>
2019-07-13 17:05:49 +0100

    chore: update and fix deployment script

--- deployment.sh
+++ deployment.sh
@@ -2,36 +2,57 @@
 
 sudo apt-get update && sudo apt-get upgrade -y
 
-sudo apt-get install python3.7-pip -y
-sudo apt-get install python3.7 -y
-sudo apt-get install libpq-dev -y
-sudo apt-get install postgresql -y
-sudo apt-get install postgresql-contrib -y
-sudo apt-get install nginx -y
-sudo apt-get install git -y
+echo "Installing python...."
+{
+	sudo apt-get install python3-pip -y
+	sudo apt-get install python3.7 -y
+	echo "Python Installed :)"
+} || {
+	echo "Python installation failed"
+}
 
-echo 'export PATH="${HOME}/.local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-sudo python3.7 -m pip install --user pipenv
+echo "Installing pipenv..."
+{
+	echo 'export PATH="${HOME}/.local/bin:$PATH"' >> ~/.bashrc
+	source ~/.bashrc
+	sudo python3.7 -m pip install --user pipenv
+	echo "Pipenv installed :)"
+} || {
+	echo "Pipenv installation failed"
+}
 
-sudo apt-get install wget
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-source ~/.bashrc
+echo "Installing nodejs"
+{
+	sudo apt-get install wget
+	wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+	source ~/.bashrc
+	nvm install 11.12.20
+	echo "Node installed :)"
+} || {
+	echo "Nodejs installation failed"
+}
 
-nvm install 11.12.20
-
-make pipenv-install
-pipenv shell
-make migrate
-make start-gunicorn
-
+echo "Installing client dependencies"
 make npm-install
+echo "Dependencies installed :)"
+
+echo "building client"
 make build-client
+echo "client built :)"
+
+echo "Installing server depenedencies"
+	make pipenv-install
+	pipenv run make migrate
+	pipenv run start-gunicorn
+echo "Gunicorn server started"
+
+echo "Installing Nginx"
+sudo apt-get install nginx -y
 sudo touch /etc/nginx/sites-available/favorite_things
 sudo ln -s /etc/nginx/sites-available/favorite_things /etc/nginx/sites-enabled/favorite_things
 
-cp nginx.conf /etc/nginx/nginx.conf
-cp favorite_things /etc/nginx/sites-available/favorite-things
-
+sudo cp nginx.conf /etc/nginx/nginx.conf
+sudo cp favorite_things /etc/nginx/sites-available/favorite-things
 sudo nginx -t
 sudo service nginx restart
+
